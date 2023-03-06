614e40a11b60d0a94fa2e4349e8fa227282771c9
Paulex <57707777+PaulexV@users.noreply.github.com>
2023-03-06 22:51:05 +0100

    Authentication added (#1)
    authored-by: Zephyor

---
+++ src/AuthProvider.tsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react"
+
+
+export const AuthContext = createContext({} as {user:object | null, setUser: (user:object) => void, error:string | null, setError:(error:string |null) => void})
+
+const AuthProvider = ({ children }: any) => {
+    const [user, setUser] = useState<object | null>(JSON.parse(localStorage.getItem('@user') as string) ?? null)
+    const [error, setError] = useState<string | null>(null)
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, error, setError }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
